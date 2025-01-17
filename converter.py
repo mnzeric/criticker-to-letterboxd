9cddc801198225bb3b3318fefa3286deb4e543ef
Kwuang Tang <arctangent20@yahoo.com>
2025-01-17 16:50:04 -0500

    i

--- converter.py
+++ converter.py
@@ -1,20 +1,39 @@
 import pandas as pd
 import datetime
+import math
 
-df = pd.read_csv('ratings.csv',sep=',',encoding='UTF-8')
+rating_file = "./data/Criticker Ratings Jan 17 2025.csv"
+output_file = "./data/to_letterboxd.csv"
 
-#!!! SKIP OR EDIT this to use rating (1-5) !!!, converting my rating format (0-10) for letterboxd rating10 converter
-df['Score'] = df['Score'].apply(lambda x: 1 if x == 0 else x)
+df = pd.read_csv(rating_file, sep=",", encoding="UTF-8")
 
-#Step added to remove extra spaces in column names
+# converts from 1-100 to 1-10
+df["Score"] = df["Score"].apply(lambda x: max(1, math.ceil(x / 10)))
+
+# Step added to remove extra spaces in column names
 df.columns = list(map(lambda x: x.strip(), df.columns))
 
-#Rearranging columns
-df = df[['IMDB ID', 'Film Name', 'Year', 'Score', 'Date Rated', 'Mini Review']]
+# Rearranging columns
+df = df[["IMDB ID", "Film Name", "Year", "Score", "Date Rated", "Mini Review"]]
 
-df.rename(columns={'IMDB ID':'imdbID', 'Film Name':'Title', 'Year':'Year', 'Score':'Rating10', 'Date Rated':'WatchedDate', 'Mini Review':'Review'}, inplace=True)
+# Rename columns to match Letterboxd format
+df.rename(
+    columns={
+        "IMDB ID": "imdbID",
+        "Film Name": "Title",
+        "Year": "Year",
+        "Score": "Rating10",
+        "Date Rated": "WatchedDate",
+        "Mini Review": "Review",
+    },
+    inplace=True,
+)
 
-#Converting date format
-df['WatchedDate'] = df['WatchedDate'].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d %H:%M').strftime("%Y-%m-%d"))
+# Converting date format to YYYY-MM-DD
+df["WatchedDate"] = df["WatchedDate"].apply(
+    lambda x: datetime.datetime.strptime(x, "%Y-%m-%d %H:%M").strftime("%Y-%m-%d")
+)
 
-df.to_csv('to_letterboxd.csv', index=False)
+df.to_csv(output_file, index=False)
+
+print(f"Output file created: {output_file}")
